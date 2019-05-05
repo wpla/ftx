12067e139c272351a9546f2a0e5e77e62c3b4572
Nishad Singh <nishadsingh@berkeley.edu>
2019-05-05 15:28:41 +0800

    Add REST authentication sample code

---
+++ authentication.py
@@ -0,0 +1,48 @@
+from requests import Request, Session
+import hmac
+
+class FtxClient:
+    _ENDPOINT = 'https://ftexchange.com/api/'
+
+    def __init__(self) -> None:
+        self._session = Session()
+        self._api_key = '' # TODO: Place your API key here
+        self._api_secret = '' # TODO: Place your API secret here
+
+    def _get(self, path: str, params: Optional[Dict[str, Any]] = None) -> Any:
+        return self._request('GET', path, params=params)
+
+    def _post(self, path: str, params: Optional[Dict[str, Any]] = None) -> Any:
+        return self._request('POST', path, json=params)
+
+    def _delete(self, path: str) -> Any:
+        return self._request('DELETE', path)
+
+    def _request(self, method: str, path: str, **kwargs) -> Any:
+        request = Request(method, self._ENDPOINT + path, **kwargs)
+        self._sign_request(request)
+        response = self._session.send(request.prepare())
+        return self._process_response(response)
+
+    def _sign_request(self, request: Request) -> None:
+        ts = int(time.time() * 1000)
+        prepared = request.prepare()
+        signature_payload = f'{ts}{prepared.method}{prepared.path_url}'.encode()
+        if prepared.body:
+            signature_payload += prepared.body
+        signature = hmac.new(self._api_secret.encode(), signature_payload, 'sha256').hexdigest()
+        request.headers['FTX-KEY'] = self._api_key
+        request.headers['FTX-SIGN'] = signature
+        request.headers['FTX-TS'] = str(ts)
+
+    def _process_response(self, response: Response) -> Any:
+        try:
+            data = response.json()
+        except ValueError:
+            response.raise_for_status()
+            raise
+        else:
+            if not data['success']:
+                raise ApiError(data['error'])
+            return data['result']
+
