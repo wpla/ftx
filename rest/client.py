91c81904da1b36f77bb09116658cd8b2f4d8d8aa
Nishad Singh <nishadsingh@berkeley.edu>
2019-05-05 17:18:39 +0800

    Add sample code for fetching market data, private data, and order placement and cancelation

---
+++ rest/client.py
@@ -0,0 +1,81 @@
+from requests import Request, Session
+import hmac
+
+class FtxClient:
+    _ENDPOINT = 'https://ftexchange.com/api/'
+
+    def __init__(self) -> None:
+        self._session = Session()
+        self._api_key = '' # TODO: Place your API key here
+        self._api_secret = '' # TODO: Place your API secret here
+
+    def _get(self, path: str, params: Optional[Dict[str, Any]] = None) -> Any:
+        return self._request('GET', path, params=params)
+
+    def _post(self, path: str, params: Optional[Dict[str, Any]] = None) -> Any:
+        return self._request('POST', path, json=params)
+
+    def _delete(self, path: str) -> Any:
+        return self._request('DELETE', path)
+
+    def _request(self, method: str, path: str, **kwargs) -> Any:
+        request = Request(method, self._ENDPOINT + path, **kwargs)
+        self._sign_request(request)
+        response = self._session.send(request.prepare())
+        return self._process_response(response)
+
+    def _sign_request(self, request: Request) -> None:
+        ts = int(time.time() * 1000)
+        prepared = request.prepare()
+        signature_payload = f'{ts}{prepared.method}{prepared.path_url}'.encode()
+        if prepared.body:
+            signature_payload += prepared.body
+        signature = hmac.new(self._api_secret.encode(), signature_payload, 'sha256').hexdigest()
+        request.headers['FTX-KEY'] = self._api_key
+        request.headers['FTX-SIGN'] = signature
+        request.headers['FTX-TS'] = str(ts)
+
+    def _process_response(self, response: Response) -> Any:
+        try:
+            data = response.json()
+        except ValueError:
+            response.raise_for_status()
+            raise
+        else:
+            if not data['success']:
+                raise ApiError(data['error'])
+            return data['result']
+
+    def list_futures(self) -> List[dict]:
+        return self._get('futures')
+
+    def get_orderbook(self, future: str, depth: int = 100) -> dict:
+        return self._get(f'futures/{future}/orderbook', {'depth': depth})
+
+    def get_trades(self, future: str) -> dict:
+        return self._get(f'futures/{future}/trades')
+
+    def get_account_info(self) -> dict:
+        return self._get(f'account')
+
+    def get_open_orders(self) -> List[dict]:
+        return self._get(f'orders')
+
+    def place_order(self, future: str, side: str, price: float, size: float) -> dict:
+        return self._post('orders', {'future': future,
+                                     'side': side,
+                                     'price': price,
+                                     'size': size})
+
+    def cancel_order(self, order_id: str) -> dict:
+        return self._delete(f'orders/{order_id}')
+
+    def get_fills(self) -> List[dict]:
+        return self._get(f'fills')
+
+    def get_balances(self) -> List[dict]:
+        return self._get('wallet/balances')
+
+    def get_deposit_address(self, ticker: str) -> dict:
+        return self._get(f'wallet/deposit_address/{ticker}')
+
