1589a9dac9a573a68556d841b4437028c76b7b6f
Nishad Singh <nishadsingh@berkeley.edu>
2019-12-26 20:04:12 -0800

    Add function to request quote

--- rest/otc_client.py
+++ rest/otc_client.py
@@ -22,6 +22,20 @@ class FtxOtcClient:
     def _delete(self, path: str) -> Any:
         return self._request('DELETE', path)
 
+    def request_otc_quote(self, base_currency: str, quote_currency: str, side: str,
+                          base_currency_size: Optional[float] = None,
+                          quote_currency_size: Optional[float] = None,
+                          wait_for_price: bool = True) -> Any:
+        assert (quote_currency_size is None) ^ (base_currency_size is None)
+        return self._post('otc/quotes', {
+            'baseCurrency': base_currency,
+            'quoteCurrency': quote_currency,
+            'baseCurrencySize': base_currency_size,
+            'quoteCurrencySize': quote_currency_size,
+            'waitForPrice': wait_for_price,
+            'side': side,
+        })
+
     def _request(self, method: str, path: str, **kwargs) -> Any:
         request = Request(method, self._ENDPOINT + path, **kwargs)
         self._sign_request(request, path)
