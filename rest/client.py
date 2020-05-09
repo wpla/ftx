99c9f7475d8b216e278590d9da464715406fc69b
LeeChunHao2000 <64617939+LeeChunHao2000@users.noreply.github.com>
2020-05-09 18:38:52 +0800

    Update client.py

--- rest/client.py
+++ rest/client.py
@@ -160,7 +160,7 @@ class FtxClient:
     def get_position(self, name: str, show_avg_price: bool = False) -> dict:
         return next(filter(lambda x: x['future'] == name, self.get_positions(show_avg_price)), None)
 
-    def get_all_trades(market: str, start_time: float = None, end_time: float = None) -> List:
+    def get_all_trades(self, market: str, start_time: float = None, end_time: float = None) -> List:
         limit = 5000
         ids = set()
         results = []
